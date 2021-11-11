98f4ece487339cbbb97ec18972c13a34535a9ab9
Dmitry <ddtihonov@yandex.ru>
2021-11-11 17:21:02 +0300

    Доработки первой страницы

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 
-const popup = document.querySelector('.popup');
+/*const popup = document.querySelector('.popup');
 
 
 // отурытие всех popup
@@ -17,5 +17,35 @@ function closePopup(evt) {
 
 document.querySelectorAll('.popup__close-icon').forEach (button => {
     button.addEventListener('click', closePopup);
-})
+})*/
 
+const isMobile = {
+    Android: function() {
+		return navigator.userAgent.match(/Android/i);
+	},
+	BlackBerry: function() {
+		return navigator.userAgent.match(/BlackBerry/i);
+	},
+	iOS: function() {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	},
+	Opera: function() {
+		return navigator.userAgent.match(/Opera Mini/i);
+	},
+	Windows: function() {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	any: function() {
+		return (
+            isMobile.Android() || 
+            isMobile.BlackBerry() || 
+            isMobile.iOS() || isMobile.Opera() || 
+            isMobile.Windows());
+	}
+};
+
+if (isMobile.any()) {
+    document.body.classList.add('_touch')
+} else {
+    document.body.classList.add('_pc')
+}
